a28dc2c63b7fd27ee30b78a8ecab2c9cf7dd26d4
shunsk <ok.xmonad@gmail.com>
2024-03-01 11:20:45 +0900

    initial commit

---
+++ R/format_check.R
@@ -0,0 +1,57 @@
+# format_pop04
+# 令和３年（２０２１年）１１月以降
+# [1,2]に日付を含んだ文字列
+# シートは１枚
+# 列名に準じる行4行め 17列
+#
+# format_pop03
+# 令和２年（２０２０年）１１月から令和３年（２０２１年）９月まで
+# [1,2]に日付を含んだ文字列
+# シートは１枚
+# 列名に準じる行4行め 14列
+#
+# format_pop02
+# 平成２７年（２０１５年）１１月から令和２年（２０２０年）９月まで
+# 列名に準じる行??11列
+#
+# format_cens 令和２年（２０２０年）１０月
+# 列名に準じる行?? 4列
+#
+# format_5sai
+# 列名に準じる行?? 4列
+
+guess_dataformat <- function(filepath){
+  a <- openxlsx::read.xlsx(filepath, colNames = FALSE)
+
+  # format_5sai
+  # 年齢別人口
+  if(ncol(a) == 22){
+    return("format_5sai")
+  }
+
+  # format_pop04
+  # 令和３年（２０２１年）１１月以降
+  if(ncol(a) == 17){
+    return("format_pop04")
+  }
+
+  # format_pop03
+  #令和２年（２０２０年）１１月から令和３年（２０２１年）９月まで
+  if(ncol(a) == 12){
+    return("format_pop03")
+  }
+
+  # format_cens 令和２年（２０２０年）１０月
+  if(ncol(a) == 4){
+    return("format_pop_cens2020")
+  }
+
+  # format_h27
+  # 平成２７年（２０１５年）１１月から令和２年（２０２０年）９月まで
+  if(ncol(a) == 11){
+    return("format_pop02")
+  }
+
+  return("format_unknown")
+
+}
---
+++ R/urls_maker.R
@@ -0,0 +1,130 @@
+#####################################################################
+# ダウンロードデータのURLのベクトルを返す関数群
+# データの種類毎に分ける
+#
+# 令和2年（2020年）11月1日から現在
+#  pop_new_urls()
+#
+# 令和2年（2020年）10月1日
+#  pop_2020_urls()
+#  1ファイル
+#
+# 平成27年（2015年）11月1日 から 令和2年（2020年）9月1日
+#  pop_old_urls()
+#  6ファイル
+#
+# 年齢別人口に関するデータのURL
+# pop_5age_urls()
+#
+###################################################################
+
+#//////////////////////////////////////////////////////////////////
+# popデータファイルURLのベクトルを作成するルーチン
+#//////////////////////////////////////////////////////////////////
+
+# 令和2年（2020年）11月1日から現在
+#  pop_new_urls()
+pop_new_urls <- function(){
+
+  # 2024現在の大阪府の人口データ（月次）配布サイト（新しい）
+  target_page <-
+    "https://www.pref.osaka.lg.jp/toukei/jinkou/jinkou-xlslist.html"
+
+  my_prefix <- "https://www.pref.osaka.lg.jp"
+
+  #https://www.pref.osaka.lg.jp/attach/3387/00014690/jk20220801.xlsx
+  #https://www.pref.osaka.lg.jp/attach/3387/00014690/kakutei_jk20210201.xlsx
+  my_filename_pattern = "jk20[0-9]{2}[0-9]{2}[0-9]{2}.*\\.xlsx$"
+
+  links <-
+    dalo::dalo_fetch_links(url = target_page,
+                           selector = "tbody td",
+                           filename_pattern = my_filename_pattern,
+                           encoding = "shift_jis",
+                           prefix = my_prefix)
+  return(links)
+}
+
+# 令和2年（2020年）10月1日
+#  pop_2020_urls()
+pop_2020_urls <- function(){
+
+  # 2020 国勢調査の大阪府版詳細ファイル
+  target_page <-
+    "https://www.pref.osaka.lg.jp/toukei/top_portal/kokucho.html"
+
+  my_prefix <- "https://www.pref.osaka.lg.jp"
+
+  ## https://www.pref.osaka.lg.jp/attach/42622/00426107/R2kokutyo_osakahu_kakuhou_syousai.xlsx
+  my_filename_pattern = "R2.*_syousai\\.xlsx$"
+
+  links <-
+    dalo::dalo_fetch_links(url = target_page,
+                           selector = "tbody td",
+                           filename_pattern = my_filename_pattern,
+                           encoding = "shift_jis",
+                           prefix = my_prefix)
+  return(links)
+
+}
+
+# 平成27年（2015年）11月1日 から 令和2年（2020年）9月1日
+#  pop_old_urls()
+pop_old_urls <- function(){
+
+  # 2024現在の大阪府の人口データ（月次）配布サイト（古い）
+  target_page <-
+    "https://www.pref.osaka.lg.jp/toukei/jinkou/jinkou-h.html"
+
+  my_prefix <- "https://www.pref.osaka.lg.jp"
+
+  # https://www.pref.osaka.lg.jp/attach/3387/00040392/hosei202001_09.xlsx
+  my_filename_pattern = "hosei20[0-9]{2}[0-9]{2}_[0-9]{2}.*\\.xlsx$"
+
+  links <-
+    dalo::dalo_fetch_links(url = target_page,
+                           selector = "tbody td",
+                           filename_pattern = my_filename_pattern,
+                           encoding = "shift_jis",
+                           prefix = my_prefix)
+  return(links)
+
+}
+
+# 年齢別人口に関するデータのURL
+# pop_5age_urls()
+pop_5age_urls <- function(){
+
+  new_target_page <-
+    "https://www.pref.osaka.lg.jp/toukei/jinkou/jinkou-xlslist.html"
+
+  old_target_page <-
+    "https://www.pref.osaka.lg.jp/toukei/jinkou/jinkou-h.html"
+
+  my_prefix <- "https://www.pref.osaka.lg.jp"
+
+  # https://www.pref.osaka.lg.jp/attach/3387/00040392/hosei202001_09.xlsx
+  my_filename_pattern = "5sai.*\\.xlsx$"
+
+  # 新しいデータに関するページ
+  newdata_link <-
+    dalo::dalo_fetch_links(url = new_target_page,
+                           selector = "tbody td",
+                           filename_pattern = my_filename_pattern,
+                           encoding = "shift_jis",
+                           prefix = my_prefix)
+
+  # 古いデータに関するページ
+  olddata_link <-
+    dalo::dalo_fetch_links(url = old_target_page,
+                           selector = "tbody td",
+                           filename_pattern = my_filename_pattern,
+                           encoding = "shift_jis",
+                           prefix = my_prefix)
+
+  # 新しい、古いデータの統合
+  ans_link <- c(newdata_link,olddata_link)
+
+  return(ans_link)
+}
+
